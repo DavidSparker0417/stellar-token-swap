9ae5cfa87d0bdbea2eeb3edb6306650f959e81c1
DavidSparker <davidsparker0417@gmain.com>
2024-04-18 03:26:02 +0900

    modified:   soroban_token_swap_frontend/src/pages/Main.tsx

--- soroban_token_swap_frontend/src/pages/Main.tsx
+++ soroban_token_swap_frontend/src/pages/Main.tsx
@@ -52,15 +52,11 @@ async function executeTransaction(accKeypair: StellarSdk.Keypair, operation: Ste
 
     const transaction = await server.prepareTransaction(transaction0);
     // transaction.sign(accKeypair);
-    console.log(`[DAVID] Signing transaction xdr:: ${transaction.toXDR()}`);
-    console.log(`[DAVID] transaction hash:: ${transaction.hash().toString('hex')}`);
     const signedXDR = await freighter.signTransaction(transaction.toXDR(), {
         networkPassphrase: StellarSdk.Networks.FUTURENET,
     });
-    // const signature = "AAAAAgAAAACNj6RokCqI7lD9vPqDqSpSdRsWkBhz0JAmnYMOS5IQ+gACDVAAEUdSAAAAHQAAAAEAAAAAAAAAAAAAAABmH/8ZAAAAAQAAAAdUZXN0aW5nAAAAAAEAAAAAAAAAGAAAAAAAAAABhrqt6lL687vtrGqUgIbrDXsRagIgJoeoX740/8DpAV0AAAAHc2V0X2ZlZQAAAAACAAAAAwAAABkAAAASAAAAAAAAAADdHMMv9dP0nPm3npbOP1hK2e9LZj90+mupSpH46FPLgQAAAAAAAAABAAAAAAAAAAEAAAAH+giXrjInL8IwsaElhNDsibVZgVRqhbKcUXBMdjKQgA8AAAABAAAABgAAAAGGuq3qUvrzu+2sapSAhusNexFqAiAmh6hfvjT/wOkBXQAAABQAAAABAHWafQAAM2gAAAu8AAAAAAACDOwAAAABS5IQ+gAAAEAj7s6pKXrlQNxk5ciuMwq7nyWumikTqiJbn0o9rIgiR9tx0VLhVvuEwDlD5E5i8EBG1UtpirOpbJ3VSXpW6IAJ";
     const txEnvelope = StellarSdk.xdr.TransactionEnvelope.fromXDR(signedXDR, 'base64');
     const tx = new StellarSdk.Transaction(txEnvelope, StellarSdk.Networks.FUTURENET);
-    // transaction.signHashX(signature);
     try {
         const response = await server.sendTransaction(tx);
         console.log('Sent! Transaction Hash:', response.hash);
